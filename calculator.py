284c28772fd4ef6c4c5390726af73bcdf3401a65
GregRhoulhac <greg.rhoulhac@gmail.com>
2018-01-15 23:38:42 -0600

    Test for adding one number

--- calculator.py
+++ calculator.py
@@ -3,3 +3,5 @@ class Calculator(object):
     def add(self, x):
         if x == '':
             return 0
+        else:
+            return int(x)
--- test_calculator.py
+++ test_calculator.py
@@ -4,7 +4,13 @@ from calculator import Calculator
 
 
 class TestAddFunction(TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
     def test_adding_empty_string_returns_zero(self):
-        calc = Calculator()
-        add = calc.add('')
+        add = self.calc.add('')
         self.assertEqual(0, add)
+
+    def test_adding_one_number_returns_the_same_number(self):
+        add = self.calc.add('1')
+        self.assertEqual(1, add)
